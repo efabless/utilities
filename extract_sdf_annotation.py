e9c5ed8af5d69ae10aac9b3c3299655e5eda0d00
Marwan Abbas <67271180+marwaneltoukhy@users.noreply.github.com>
2022-04-26 15:48:23 +0200

    Create extract_sdf_annotation.py

---
+++ extract_sdf_annotation.py
@@ -0,0 +1,24 @@
+import argparse
+ 
+ from attr import has
+ 
+ parser = argparse.ArgumentParser(
+     description="extracts the list of modules for sdf annotation"
+ )
+ 
+ parser.add_argument("--input", "-i", required=True, help="rtl verilog file")
+ parser.add_argument("--output", "-o", required=True, help="includes file")
+ 
+ args = parser.parse_args()
+ input = args.input
+ output = args.output
+ 
+ pattern = " ("
+ sdf_path = "../../../sdf/"
+ 
+ with open(input, "r") as f:
+     for line in f:
+         if pattern in line: 
+             if "module" not in line:
+                 module = line.split()
+                 print("$sdf_annotate(\"" + sdf_path + module[0] + ".sdf\", uut.mprj.fpga_core_uut." + module[1] + ") ;")
