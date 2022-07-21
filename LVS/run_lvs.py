f90eb4c89b33a8ab42cb27c2a321cc9aea734467
Marwan Abbas <marwaneltoukhy@aucegypt.edu>
2022-07-21 18:22:47 +0200

    added support for mag and maglef

--- LVS/run_lvs.py
+++ LVS/run_lvs.py
@@ -8,7 +8,7 @@ import argparse
 
 # This function is to extract gds to spice calling extract_gds.tcl script
 # arguments to the function:
-#               ext_file --> gds file absolute path to get extracted
+#               ext_file   --> gds file absolute path to get extracted
 #               output_dir --> output directory
 # output of the function:
 #               creates a tmp_ext directory under the output directory and dumps the .ext files there
@@ -22,28 +22,46 @@ def extract_gds(ext_file, output_dir):
 
     subprocess.run(["magic", "-dnull", "-noconsole", "-rcfile", f"{PDK_ROOT}/{PDK}/libs.tech/magic/{PDK}.magicrc", "extract_gds.tcl"])
 
-# This function is to run LVS on spice generated with gds, and verilog GL netlist
+# This function is to extract mag or maglef to spice calling extract_mag.tcl script
 # arguments to the function:
-#               netlist_1 --> spice netlist
-#               netlist_2 --> verilog GL netlist
+#               ext_file   --> mag file absolute path to get extracted
 #               output_dir --> output directory
+# output of the function:
+#               creates a tmp_ext directory under the output directory and dumps the .ext files there
+#               generates the .spice netlist under the output directory
+#
+# Date created: 07/21/2022
+
+def extract_mag(ext_file, output_dir):
+    os.environ['ext_inp1'] = ext_file
+    os.environ['ext_out'] = output_dir
+
+    subprocess.run(["magic", "-dnull", "-noconsole", "-rcfile", f"{PDK_ROOT}/{PDK}/libs.tech/magic/{PDK}.magicrc", "extract_mag.tcl"])
+
+# This function is to run LVS on extracted spice, and verilog GL netlist
+# arguments to the function:
+#               netlist_1  --> spice netlist
+#               netlist_2  --> verilog GL netlist
+#               output_dir --> output directory
+#               lvs_type   --> tyoe of lvs done: gds or mag
 # output of the function:
 #               Runs LVS and generates output log under the output directory
 #
 # Date created: 07/21/2022
 
-def run_lvs_gds(netlist_1, netlist_2, output_dir):
+def run_lvs(netlist_1, netlist_2, output_dir, lvs_type):
+    if lvs_type == "gds":
+        os.environ['MAGIC_EXT_USE_GDS'] = "1"
     os.environ['NETGEN_COLUMNS'] = "60"
-    os.environ['MAGIC_EXT_USE_GDS'] = "1"
     netlist1_name = os.path.basename(netlist_1).split('.')[0]
 
     subprocess.run(['netgen', '-batch', 'lvs', f'{output_dir}/{netlist1_name}-gds-extracted.spice {netlist1_name}',
 		f'{netlist_2} {netlist1_name}', f'{PDK_ROOT}/{PDK}/libs.tech/netgen/{PDK}_setup.tcl', 
-        f'{output_dir}/{netlist1_name}-gds-vs-verilog.out'])
+        f'{output_dir}/{netlist1_name}-{lvs_type}-vs-verilog.out'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process LVS check. Layout will always be on the left')
-    parser.add_argument('-i', '--input', help="takes two input files", required=True, nargs=2)
+    parser.add_argument('-i', '--input', help="input files to run LVS on them. Takes two input files", required=True, nargs=2)
     parser.add_argument('-o', '--output', help="output file", required=True)
     args = parser.parse_args()
     PDK_ROOT=os.getenv('PDK_ROOT')
@@ -66,8 +84,18 @@ if __name__ == "__main__":
         lvs_type = "gds"
         if input1_type == "gds":
             extract_gds(input1, output)
-            run_lvs_gds(input1, input2, output)
+            run_lvs(input1, input2, output, lvs_type)
         elif input2_type == "gds":
             extract_gds(input2, output)
-            run_lvs_gds(input2, input1, output)
+            run_lvs(input2, input1, output, lvs_type)
+
+    if input1_type == "mag" or input2_type == "mag":
+        lvs_type = "mag"
+        if input1_type == "mag":
+            extract_mag(input1, output)
+            run_lvs(input1, input2, output, lvs_type)
+        elif input2_type == "mag":
+            extract_mag(input2, output)
+            run_lvs(input2, input1, output, lvs_type)
+    
 
