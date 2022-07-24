546cf85863754f3727037633180176e0c168bef9
Marwan Abbas <marwaneltoukhy@aucegypt.edu>
2022-07-24 11:29:42 +0200

    code cleanup script will now detect if any file of the two can be extracted and will extract and run LVS on it

--- LVS/run_lvs.py
+++ LVS/run_lvs.py
@@ -6,123 +6,109 @@ import subprocess
 import os
 import argparse
 
-def extract_gds(ext_file, output_dir):
-    """Extract gds to spice calling extract_gds.tcl script
+def extract(file_dict, output_dir):
+    """extract spice using magic
 
     Args:
-        ext_file (string): gds file absolute path to get extracted
+        file_dict (dict): dictionary of input file
         output_dir (string): output directory
     Output:
         creates a tmp_ext directory under the output directory and dumps the .ext files there
         generates the .spice netlist under the output directory
     History:
-        created 07/21/2022
+        created 07/24/2022
     """
-
-    os.environ['ext_inp1'] = ext_file
+    os.environ['ext_inp1'] = file_dict.get("file_path")
     os.environ['ext_out'] = output_dir
 
-    subprocess.run(["magic", "-dnull", "-noconsole", "-rcfile", f"{PDK_ROOT}/{PDK}/libs.tech/magic/{PDK}.magicrc", "extract_gds.tcl"])
+    if file_dict.get("file_extraction") is True:
+        subprocess.run(["magic", "-dnull", "-noconsole", "-rcfile", f"{PDK_ROOT}/{PDK}/libs.tech/magic/{PDK}.magicrc", f"extract_{file_dict['file_type']}.tcl"])
+    elif file_dict.get("file_extraction") is None:
+        raise TypeError(f"LVS on {file_dict['file_type']} files not supported")
 
-def extract_mag(ext_file, output_dir):
-    """Extract mag or maglef to spice calling extract_mag.tcl script
 
-    Args:
-        ext_file (string): mag file absolute path to get extracted
-        output_dir (string): output directory
-    Output:
-        creates a tmp_ext directory under the output directory and dumps the .ext files there
-        generates the .spice netlist under the output directory
-    History:
-        created 07/21/2022
-    """
-
-    os.environ['ext_inp1'] = ext_file
-    os.environ['ext_out'] = output_dir
-
-    subprocess.run(["magic", "-dnull", "-noconsole", "-rcfile", f"{PDK_ROOT}/{PDK}/libs.tech/magic/{PDK}.magicrc", "extract_mag.tcl"])
-
-def run_lvs(netlist_1, netlist_2, output_dir, lvs_type):
-    """AI is creating summary for run_lvs
+def run_lvs(netlist_1, netlist_2, output_dir):
+    """run generic LVS
 
     Args:
-        netlist_1 (string): spice netlist
-        netlist_2 (string): verilog GL netlist
+        netlist_1 (dict): netlist 1 dictionary
+        netlist_2 (dict): netlist 2 dictionary
         output_dir (string): output directory
-        lvs_type (string): type of lvs done: gds or mag
     Output:
         Runs LVS and generates output log under the output directory
     History:
         created 07/21/2022
     """
 
-    if lvs_type == "gds":
+    if netlist_1.get("file_type") == "gds" or netlist_2.get("file_type") == "gds":
         os.environ['MAGIC_EXT_USE_GDS'] = "1"
     os.environ['NETGEN_COLUMNS'] = "60"
-    netlist1_name = os.path.basename(netlist_1).split('.')[0]
+    netlist1_name = os.path.basename(netlist_1.get("file_path")).split('.')[0]
+    netlist2_name = os.path.basename(netlist_2.get("file_path")).split('.')[0]
 
     netgen_setup_file = f"{PDK_ROOT}/{PDK}/libs.tech/netgen/{PDK}_setup.tcl"
-    if lvs_type == "gds" or lvs_type == "mag":
-        spice_file = f"{output_dir}/{netlist1_name}-{lvs_type}-extracted.spice"
+    if netlist_1.get("file_extraction") is True:
+        spice_file1 = f"{output_dir}/{netlist1_name}-{netlist_1['file_type']}-extracted.spice"
+    else:
+        spice_file1 = netlist_1.get("file_path")
+    
+    if netlist_2.get("file_extraction") is True:
+        spice_file2 = f"{output_dir}/{netlist2_name}-{netlist_2['file_type']}-extracted.spice"
+    else:
+        spice_file2 = netlist_2.get("file_path")
 
     subprocess.run(['netgen', '-batch', 'lvs', 
-        f'{spice_file} {netlist1_name}',
-		f'{netlist_2} {netlist1_name}', 
+        f'{spice_file1} {netlist1_name}',
+		f'{spice_file2} {netlist1_name}', 
         f'{netgen_setup_file}', 
-        f'{output_dir}/{netlist1_name}-{lvs_type}-vs-verilog.out'])
+        f'{output_dir}/{netlist1_name}-{netlist_1["file_type"]}-vs-verilog.out'])
 
-def run_ext(input1, input2, output):
-    """Chooses which extraction to run based on file extension
-
-    Args:
-        input1 (string): path to input file 1
-        input2 (string): path to input file 2
-        output (string): path to output file
-    History:
-        created 07/21/2022
-    """
-    input1_type = get_file_type(input1)
-    input2_type = get_file_type(input2)
-
-    if input1_type == "gds" or input2_type == "gds":
-        lvs_type = "gds"
-        if input1_type == "gds":
-            extract_gds(input1, output)
-            run_lvs(input1, input2, output, lvs_type)
-        elif input2_type == "gds":
-            extract_gds(input2, output)
-            run_lvs(input2, input1, output, lvs_type)
-
-    if input1_type == "mag" or input2_type == "mag":
-        lvs_type = "mag"
-        if input1_type == "mag":
-            extract_mag(input1, output)
-            run_lvs(input1, input2, output, lvs_type)
-        elif input2_type == "mag":
-            extract_mag(input2, output)
-            run_lvs(input2, input1, output, lvs_type)
-
-def get_file_type(file):
-    """find file type
+def set_ext_files(file_type):
+    """checks if the file can be extracted
 
     Args:
-        file (string): file to get its type
+        file_type (string): checks if the file type can be extracted
 
     Returns:
-        string: file type
+        [bool]: bool to reflect if the file type can be extracted
     """
-    file_ext = os.path.basename(file).split('.')[1]
+    if file_type == "gds" or file_type == "mag":
+        file_ext = True
+    elif file_type == "spice" or file_type == "v":
+        file_ext = False
+    else:
+        file_ext = None
 
-    if file_ext == "v":
-        file_type = "verilog"
-    elif file_ext == "spice":
-        file_type = "spice"
-    elif file_ext == "gds":
-        file_type = "gds"
-    elif file_ext == "mag":
-        file_type = "mag"
+    return file_ext
     
-    return file_type
+
+def set_db(file1_path, file2_path):
+    """sets the tuples for the file inputs
+
+    Args:
+        file1 (string): path to first input file
+        file2 (string): path to second input file
+
+    Returns:
+        tuple (string, bool): tuple containing (file1_path, if the file1 can be extracted) and (file2_path, if the file1 can be extracted)
+    History:
+        created 07/24/2022
+    """
+    file1_type = os.path.basename(file1_path).split('.')[1]
+    file2_type = os.path.basename(file2_path).split('.')[1]
+    
+    file1_db = {
+        "file_path" : file1_path,
+        "file_type" : file1_type,
+        "file_extraction" : set_ext_files(file1_type)
+    }
+    file2_db = {
+        "file_path" : file2_path,
+        "file_type" : file2_type,
+        "file_extraction" : set_ext_files(file2_type)
+    }
+
+    return file1_db, file2_db
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process LVS check. Layout will always be on the left')
@@ -142,7 +128,10 @@ if __name__ == "__main__":
         # directory already exists
         pass
 
-    run_ext(input1, input2, output)
+    input1_db, input2_db = set_db(input1, input2)
+    extract(input1_db, output)
+    extract(input2_db, output)
+    run_lvs(input1_db, input2_db, output)
 
     
     
